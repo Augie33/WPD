889b7fcc0af01875e2b19a59982c2e701ea5c1af
Dhari <dmakwt@gmail.com>
2021-09-30 16:03:31 -0500

    add more fields for the Case Schema

--- wpd-api/src/models/case.js
+++ wpd-api/src/models/case.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
-
+const validator = require('validator');
 
 const CaseSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, 'Please add a title'],
+    },
+    description: {
+      type: String,
+    },
+    url: {
+      type: String,
+      validate(value){
+        if(!validator.isURL(value)){
+          throw new Error('Please correct url');
+        }
+      }
+    },
+    urlPDF: {
+      type: String,
+      validate(value){
+        if(!validator.isURL(value)){
+          throw new Error('Please correct urlPDF');
+        }
+      }
     }
   });
 
