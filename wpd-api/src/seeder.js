e9f9f022bffa0e3757bcdac32c319e79abc73ff1
Dhari <dmakwt@gmail.com>
2021-09-30 18:54:26 -0500

    add seeder

--- wpd-api/src/seeder.js
+++ wpd-api/src/seeder.js
@@ -4,8 +4,11 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 
 
+console.log(__dirname);
+
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: '../config/config.env' });
+
 
 
 // Load models
@@ -13,15 +16,53 @@ const Case = require('./models/case');
 const User = require('./models/User');
 
 
-
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 
 
 
+// Read JSON files
+const cases = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/cases.json`, 'utf-8')
+);
+
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Case.create(cases);
+    await User.create(users);
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Case.deleteMany();
+    await User.deleteMany();
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
