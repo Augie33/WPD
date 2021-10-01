f4f06c511513ec58bd8678995075a76251686719
Dhari <dmakwt@gmail.com>
2021-09-30 19:13:33 -0500

    remove some middleware from the case routes

--- wpd-api/src/routes/case.js
+++ wpd-api/src/routes/case.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const advancedResults = require('advanced-results');
-const {getCases, getCase, getCaseByCaseNumber,createCase, updateCase, deleteCase,} = require('../controllers/case')
+const {
+  getCases,
+  getCase,
+  getCaseByCaseNumber,
+  createCase,
+  updateCase,
+  deleteCase,
+} = require('../controllers/case');
 
 const Case = require('../models/case');
-
-
-
 
 const router = express.Router();
 
 const { protect, authorize } = require('../middleware/auth');
 
+router.get('/:id', getCase);
+
 router.use(protect);
 
-
-router.get('/', authorize('police'),advancedResults(Case), getCases);
-router.post('/', authorize('police'), createCase);
-router.get('/:id', getCase);
-router.get('/number/:caseNumber', authorize( 'police'), getCaseByCaseNumber);
-router.put('/:id', authorize('police'), updateCase);
-router.delete('/:id', authorize('police'), deleteCase);
-
+router.get('/', advancedResults(Case), getCases);
+router.post('/', createCase);
+router.get('/number/:caseNumber', getCaseByCaseNumber);
+router.put('/:id', updateCase);
+router.delete('/:id', deleteCase);
 
 module.exports = router;
