13fb9bd818f3e03dc01e0bd3f72ac0dc2cd15895
Elina <81105768+elinatdo@users.noreply.github.com>
2021-10-06 22:20:04 -0500

    Update case.js

--- wpd-api/src/models/case.js
+++ wpd-api/src/models/case.js
@@ -1,7 +1,9 @@
+//Using Node.js require()
 const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const validator = require('validator');
 
+//Fields needed for each case
 const CaseSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -21,6 +23,7 @@ const CaseSchema = new mongoose.Schema({
   },
 });
 
+//Auto-increments the case number to keep track of the cases and make sure there are no duplicates
 CaseSchema.plugin(AutoIncrement, { inc_field: 'caseNumber' });
 
 module.exports = mongoose.model('Case', CaseSchema);
