b4cf47406826922d329dd697855d3ce0052b87bb
Elina <81105768+elinatdo@users.noreply.github.com>
2021-10-07 15:23:42 -0500

    Update seeder.js

--- wpd-api/src/seeder.js
+++ wpd-api/src/seeder.js
@@ -54,7 +54,7 @@ const deleteData = async () => {
   }
 };
 
-//'-i' to import and '-d' to delete
+//'-i' import and '-d' delete
 if (process.argv[2] === '-i') {
   importData();
 } else if (process.argv[2] === '-d') {
