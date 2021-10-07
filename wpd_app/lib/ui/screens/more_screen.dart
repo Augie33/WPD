afb577a71e9159053dfb9254767dc8c71a791680
Dhari <dmakwt@gmail.com>
2021-10-07 06:10:16 -0500

    add more screen

--- wpd_app/lib/ui/screens/more_screen.dart
+++ wpd_app/lib/ui/screens/more_screen.dart
@@ -1,13 +1,142 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wpd_app/view_models/auth_state_viewmodel.dart';
 
-class MoreScreen extends StatelessWidget {
+class MoreScreen extends HookWidget {
   const MoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final authStateViewModel = useProvider(AuthStateViewModelProvider.provider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('More'),
+      ),
+      body: ListView(
+        children: [
+          if (authStateViewModel.myUser != null &&
+              authStateViewModel.myUser!.role == 'admin')
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Admin',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(color: Colors.grey[500]),
+                    ),
+                  ),
+                ),
+                SettingTile(
+                  label: 'Create a new account',
+                  icon: Icons.person_add,
+                  onTap: () {},
+                ),
+                SettingTile(
+                  label: 'Show all accounts',
+                  icon: Icons.people,
+                  onTap: () {},
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Settings',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1!
+                  .copyWith(color: Colors.grey[500]),
+            ),
+          ),
+          SettingTile(
+            label: 'Help',
+            icon: Icons.help,
+            onTap: () {},
+          ),
+          const SettingTileWithSubtitle(
+            label: 'About APP',
+            subtitle: '1.0',
+            icon: Icons.info,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SettingTile extends StatelessWidget {
+  const SettingTile({
+    Key? key,
+    required this.icon,
+    required this.label,
+    this.onTap,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String label;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: onTap,
+        leading: Icon(
+          icon,
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text(
+          label,
+          style: Theme.of(context).textTheme.headline3,
+        ),
+      ),
+    );
+  }
+}
+
+class SettingTileWithSubtitle extends StatelessWidget {
+  const SettingTileWithSubtitle({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.subtitle,
+    this.onTap,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String label;
+  final String subtitle;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: onTap,
+        leading: Icon(
+          icon,
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text(
+          label,
+          style: Theme.of(context).textTheme.headline3,
+        ),
+        trailing: Text(
+          subtitle,
+          style: Theme.of(context)
+              .textTheme
+              .headline3!
+              .copyWith(color: Colors.grey[400]),
+        ),
       ),
     );
   }
